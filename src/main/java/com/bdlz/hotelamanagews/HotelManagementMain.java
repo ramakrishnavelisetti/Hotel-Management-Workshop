e267ffb278b444be8f6b129d08395dccd73d17e1
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-22 08:11:25 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/hotelamanagews/HotelManagementMain.java
@@ -0,0 +1,7 @@
+package com.bdlz.hotelamanagews;
+
+public class HotelManagementMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Management Program");
+    }
+}
